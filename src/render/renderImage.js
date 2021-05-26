e93ba0fe8d3fcc014e724152730b0c9951351444
Elanis <elanis@hotmail.com>
2021-05-26 15:02:19 +0200

    Canvas image render & object

---
+++ src/render/renderImage.js
@@ -0,0 +1,28 @@
+export default function renderImage(context, element, left, top, localTileSize) {
+	if(!imgCache[element.src]) {
+		imgCache[element.src] = new Image();
+		imgCache[element.src].src = element.src;
+	}
+
+	if(!element.sx) {
+		context.drawImage(
+			imgCache[element.src],
+			left + element.x * localTileSize,
+			top + element.y * localTileSize,
+			element.width * localTileSize,
+			element.height * localTileSize
+		);
+	} else {
+		context.drawImage(
+			imgCache[element.src],
+			element.sx,
+			element.sy,
+			element.swidth,
+			element.sheight,
+			left + element.x * localTileSize,
+			top + element.y * localTileSize,
+			element.width * localTileSize,
+			element.height * localTileSize
+		);
+	}
+}
---
+++ src/shapes/CanvasImage.js
@@ -0,0 +1,30 @@
+import CanvasObject from './CanvasObject';
+
+export default class CanvasImage extends CanvasObject {
+	/**
+	 * Represents an Image that will be created on the canvas.
+	 * Note: a cache to prevent too much loading is implemented, you can create as much
+	 * images as you want of the same source without any performance issue.
+	 *
+	 * @param      {string|number}  id 		Identifier (used in click callback)
+	 * @param      {number}			x       Left coordinate of top-left corner of picture in canvas
+	 * @param      {number}			y       Top coordinate of top-left corner of picture in canvas
+	 * @param      {number}  		width   Picture width in canvas
+	 * @param      {number}  		height  Picture height in canvas
+	 * @param      {string}      	src 	Image source link
+	 */
+	constructor({ id, x, y, width, height, src }) {
+		super(id, x, y);
+
+		this.width = width;
+		this.height = height;
+		this.src = src;
+	}
+
+	crop(x, y, width, height) {
+		this.sx = sx;
+		this.sy = sx;
+		this.swidth = swidth;
+		this.sheight = sheight;
+	}
+}
