b035ba1cb57413f7d639e5b47fc229be9e3a6a54
Elanis <elanis@hotmail.com>
2021-05-26 15:22:41 +0200

    Fix collisions detection

--- src/collisions/inCircle.js
+++ src/collisions/inCircle.js
@@ -1,14 +1,8 @@
 export default function inCircle(element, x, y, left, top, localTileSize) {
 	const distance = (
 		(x - left) * (x - left) +
-		(y - top) * (element.y - top)
+		(y - top) * (y - top)
 	);
 
-	if(distance <= ((element.radius * localTileSize) * (element.radius * localTileSize))) {
-		return {
-			id: element.id,
-			element,
-			originalEvent: e,
-		};
-	}
+	return (distance <= ((element.radius * localTileSize) * (element.radius * localTileSize)));
 }
--- src/collisions/inRect.js
+++ src/collisions/inRect.js
@@ -2,11 +2,5 @@ export default function inRect(element, x, y, left, top, localTileSize) {
 	const width = element.width * localTileSize;
 	const height = element.height * localTileSize;
 
-	if(x <= left && left <= (x + width) && y <= top && top <= (y + height)) {
-		return {
-			id: element.id,
-			element,
-			originalEvent: e,
-		};
-	}
+	return (x <= left && left <= (x + width) && y <= top && top <= (y + height));
 }
