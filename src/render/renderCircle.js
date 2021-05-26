aaec2ed92abdea4c63a29aa88179e3873d4d299e
Elanis <elanis@hotmail.com>
2021-05-26 15:15:06 +0200

    Canvas circle render & object

---
+++ src/render/renderCircle.js
@@ -0,0 +1,17 @@
+export default function renderCircle(context, element, left, top, localTileSize) {
+	context.beginPath();
+	context.arc(
+		left + element.x * localTileSize,
+		top + element.y * localTileSize,
+		element.radius * localTileSize,
+		0,
+		2 * Math.PI
+	);
+
+	if(element.fill) {
+		context.fill();
+	}
+	if(element.stroke) {
+		context.stroke();
+	}
+}
