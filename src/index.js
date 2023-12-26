eba75fe0b56a1f6f17a731b947bd324dec4ecaab
Elanis <elanis@hotmail.com>
2023-12-26 11:41:45 +0100

    Expose few gameplay helper hooks

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import Canvas2D from './Canvas2D';
+
+import preloadImages from './functions/preloadImages';
+
+import useGamepad from './hooks/useGamepad';
+import useKeyboard from './hooks/useKeyboard';
+import useMousePosition from './hooks/useMousePosition';
+import useWindowDimensions from './hooks/useWindowDimensions';
+
 import CanvasImage from './shapes/CanvasImage';
 import Circle from './shapes/Circle';
 import Polygon from './shapes/Polygon';
 import Rect from './shapes/Rect';
-import preloadImages from './functions/preloadImages';
 
 export {
 	Canvas2D,
+
+	preloadImages,
+
+	useGamepad,
+	useKeyboard,
+	useMousePosition,
+	useWindowDimensions,
+
 	CanvasImage,
 	Circle,
 	Polygon,
 	Rect,
-	preloadImages
 };
