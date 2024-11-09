d3b84a42999af5c5d5ab2bcff1ca08013cf9632d
Elanis <elanis@hotmail.com>
2024-11-09 12:17:11 +0100

    Fix collideElement() when multiple elements could match

--- src/collisions/collideElement.js
+++ src/collisions/collideElement.js
@@ -7,6 +7,8 @@ import inRect from './inRect';
 export default function collideElement(e, elements, left, top, tileSize, zoom) {
 	const localTileSize = calcTileSize(tileSize, zoom);
 
+	const validElements = [];
+
 	for(const element of elements) {
 		const x = element.x * localTileSize;
 		const y = element.y * localTileSize;
@@ -15,33 +17,61 @@ export default function collideElement(e, elements, left, top, tileSize, zoom) {
 			case 'Rect':
 			case 'CanvasImage':
 				if(inRect(element, x, y, left, top, localTileSize)) {
-					return {
+					validElements.push({
 						id: element.id,
 						element,
 						originalEvent: e,
-					};
+					});
 				}
 				break;
 			case 'Circle':
 				if(inCircle(element, x, y, left, top, localTileSize)) {
-					return {
+					validElements.push({
 						id: element.id,
 						element,
 						originalEvent: e,
-					};
+					});
 				}
 				break;
 			case 'Polygon':
 				if(inPoly(element, x, y, left, top, localTileSize)) {
-					return {
+					validElements.push({
 						id: element.id,
 						element,
 						originalEvent: e,
-					};
+					});
 				}
 				break;
 		}
 	}
 
-	return null;
+	if(validElements.length === 0) {
+		return null;
+	}
+
+	if(validElements.length === 1) {
+		return validElements[0];
+	}
+
+	validElements.sort((a, b) => {
+		if(a.zIndex > b.zIndex) {
+			return 1;
+		}
+
+		if(a.zIndex < b.zIndex) {
+			return -1;
+		}
+
+		if(a.id > b.id) {
+			return 1;
+		}
+
+		if(a.id < b.id) {
+			return -1;
+		}
+
+		return 0;
+	});
+
+	return validElements[0];
 }
