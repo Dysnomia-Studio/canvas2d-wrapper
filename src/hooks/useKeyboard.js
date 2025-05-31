3e325c2f3907a62f17fc967b6911334641acf557
Elanis <elanis@hotmail.com>
2025-05-31 16:28:14 +0200

    Fix useKeyboard() in debug mode

--- src/hooks/useKeyboard.js
+++ src/hooks/useKeyboard.js
@@ -4,14 +4,23 @@ export default function useKeyboard() {
 	const [keyboard, setKeyboard] = useState({});
 
 	useEffect(() => {
-		document.addEventListener('keydown', (e) => {
+		const keydown = (e) => {
 			keyboard[e.key] = true;
 			setKeyboard(JSON.parse(JSON.stringify(keyboard)));
-		});
-		document.addEventListener('keyup', (e) => {
+		};
+		document.addEventListener('keydown', keydown);
+
+		const keyup = (e) => {
 			keyboard[e.key] = false;
 			setKeyboard(JSON.parse(JSON.stringify(keyboard)));
-		});
+		};
+		document.addEventListener('keyup', keyup);
+
+
+		return () => {
+			document.removeEventListener('keydown', keydown);
+			document.removeEventListener('keyup', keyup);
+		};
 	}, []);
 
 	return keyboard;
