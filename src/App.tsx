2373b10d9fc083697c7ba470d0d22d7b51a010dc
Elanis <elanis@hotmail.com>
2026-08-04 17:25:55 +0200

    Fix demo code with 3.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,44 @@
 import React from 'react';
 import generateTestData from '../lib/generateTestData.ts';
 import '../lib/index.css';
-import { Canvas2D } from '../lib/main.ts';
+import { Canvas2D, Canvas2DWrapperContextProvider } from '../lib/main.ts';
 import './index.css';
 
 const elements = generateTestData();
 
 const App = () => {
 	return (
-		<Canvas2D
-			id="MyCanvas"
-			width={1200}
-			height={700}
-			minZoom={0.25}
-			maxZoom={4}
-			tileSize={1}
-			deltaLeft={100}
-			deltaTop={100}
-			onClick={(e) => {
-				console.log('Click event:', e);
-			}}
-			onRightClick={(e) => {
-				console.log('Right click event:', e);
-			}}
-			onHover={(e) => {
-				console.log('Hover event:', e);
-			}}
-			onElementMoved={(elt, x, y) => {
-				console.log('Moved element:', elt, x, y);
-			}}
-			onWheel={(e) => {
-				console.log('Wheel event:', e);
-			}}
-			onFrame={(e) => {
-				return elements;
-			}}
-			dragObjects={true}
-		/>
+		<Canvas2DWrapperContextProvider>
+			<Canvas2D
+				id="MyCanvas"
+				width={1200}
+				height={700}
+				minZoom={0.25}
+				maxZoom={4}
+				tileSize={1}
+				deltaLeft={100}
+				deltaTop={100}
+				onClick={(e) => {
+					console.log('Click event:', e);
+				}}
+				onRightClick={(e) => {
+					console.log('Right click event:', e);
+				}}
+				onHover={(e) => {
+					console.log('Hover event:', e);
+				}}
+				onElementMoved={(elt, x, y) => {
+					console.log('Moved element:', elt, x, y);
+				}}
+				onWheel={(e) => {
+					console.log('Wheel event:', e);
+				}}
+				onFrame={() => {
+					return elements;
+				}}
+				dragObjects={true}
+			/>
+		</Canvas2DWrapperContextProvider>
 	);
 };
 
